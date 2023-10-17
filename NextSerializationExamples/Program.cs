675f9d573fa37c0eda42a17826e07d26c70e732f
Erik Gabbard <egabbard@diagraph.com>
2023-10-17 13:59:02 -0500

    Removed bad code

--- NextSerializationExamples/Program.cs
+++ NextSerializationExamples/Program.cs
@@ -1,51 +1,15 @@
-﻿using NextSerialization.MessageTypes.Next;
-using NextSerialization.MessageTypes.Next.Extensions;
-using NextSerialization.MessageTypes.Next.Serialization;
+﻿using NextSerialization.MessageTypes.Next.Extensions;
 
 var inputFile = @"SampleFiles/00157.next";
 var outputFile = @"SampleFiles/00157_Serialized.next";
 
-// The following code uses the static methods of the MessageSerializer class to read and write the 
-// message XML data to/from file.
-try
-{
-    File.Delete(outputFile);
-
-    var message = MessageSerializer.ReadMessageFile<NextMessage>(inputFile);
-
-    if (message is null)
-    {
-        throw new InvalidOperationException("Unable to deserialize message data.");
-    }
-
-    Console.WriteLine("Message successfully read.");
-    Console.WriteLine($"Writing message data to {outputFile}");
-
-    MessageSerializer.WriteMessageFile<NextMessage>(message, outputFile);
-
-    Console.WriteLine("Message file successfully written.");
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
-
-Console.WriteLine("Press enter to continue");
-Console.ReadLine();
-
-// The following code uses extension methods provided for your convienience to perform the same
-// operations as above.
 try
 {
     File.Delete(outputFile);
 
     Console.WriteLine($"Reading message data from {inputFile}");
-    var message = inputFile.ReadMessageFromFile();
 
-    if (message is null)
-    {
-        throw new InvalidOperationException("Unable to deserialize message data.");
-    }
+    var message = inputFile.ReadMessageFromFile() ?? throw new InvalidOperationException("Unable to deserialize message data.");
 
     Console.WriteLine("Message successfully read.");
     Console.WriteLine($"Writing message data to {outputFile}");
