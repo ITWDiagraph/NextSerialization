5d4aecbfb2a76fe0fcfe2d9aabde87367963c427
Erik Gabbard <egabbard@diagraph.com>
2023-10-24 12:53:29 -0500

    Update test program

--- NextSerializationExamples/Program.cs
+++ NextSerializationExamples/Program.cs
@@ -1,7 +1,8 @@
-﻿using Diagraph.Message.Next.Extensions;
+﻿using Diagraph.Message.Next;
+using Diagraph.Message.Next.Serialization;
 
-var inputFile = @"SampleFiles/00157.next";
-var outputFile = @"SampleFiles/00157_Serialized.next";
+var inputFile = @"C:\Source\MessageData\convert_Serialized.next";
+var outputFile = @"C:\Source\MessageData\convert_Serialized_out.next";
 
 try
 {
@@ -9,12 +10,22 @@ try
 
     Console.WriteLine($"Reading message data from {inputFile}");
 
-    var message = inputFile.ReadMessageFromFile() ?? throw new InvalidOperationException("Unable to deserialize message data.");
+    //var messageLines = File.ReadAllLines(inputFile);
+    //var messageXml = MessageSerializer.CorrectMessageXml(messageLines);
+
+    var product = MessageSerializer.ReadMessageFile<Product>(inputFile);
+
+    var message = new NextMessage
+    {
+        Product = product
+    };
+
+    //var message = inputFile.ReadMessageFromFile() ?? throw new InvalidOperationException("Unable to deserialize message data.");
 
     Console.WriteLine("Message successfully read.");
     Console.WriteLine($"Writing message data to {outputFile}");
 
-    message.WriteMessageToFile(outputFile);
+    MessageSerializer.WriteMessageFile<Product>(message.Product, outputFile);
 
     Console.WriteLine("Message file successfully written.");
 }
